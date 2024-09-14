cbe8e01e4fe1c101eaca6c49c60d18905bfc2422
Pratheeba S <142966931+Pratheeba21@users.noreply.github.com>
2024-09-14 17:48:25 +0530

    majorityElementInArray.java

--- majorityElementInArray.java
+++ majorityElementInArray.java
@@ -1,4 +1,4 @@
-###Problem Description
+/*###Problem Description
 
 Given an array of size N, find the majority element. The majority element is the element that appears more than floor(N/2) times.
 You may assume that the array is non-empty and the majority element always exist in the array.
@@ -27,12 +27,12 @@ Example Output
 
 
 Example Explanation
-2 occurs 2 times which is greater than 3/2.
+2 occurs 2 times which is greater than 3/2.*/
 
 
 
 
-###PROGRAM:
+//PROGRAM:
 import java.util.List;
 
 public class Solution {
